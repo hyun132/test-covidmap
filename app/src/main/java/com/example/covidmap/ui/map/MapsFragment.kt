3b5b43618dfe9ff8f40db9aef2dd59656de0459e
rlaal <rlaalgus132@naver.com>
2021-07-04 17:33:33 +0900

    get my location

--- app/src/main/java/com/example/covidmap/ui/map/MapsFragment.kt
+++ app/src/main/java/com/example/covidmap/ui/map/MapsFragment.kt
@@ -1,17 +1,26 @@
 package com.example.covidmap.ui.map
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.Camera
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.covidmap.COVIDMapApplication.Companion.ApplicationContext
 import com.example.covidmap.R
 import com.example.covidmap.databinding.FragmentMapsBinding
 import com.example.covidmap.model.Center
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
@@ -20,30 +29,26 @@ import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.security.Permission
 
 class MapsFragment : Fragment() {
 
     private val viewModel: MapsViewModel by viewModels()
-//    var centerList: List<Center> = listOf()
+
+    private var currentLocation = LatLng(127.toDouble(), 127.toDouble())
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     private val REQUEST_CODE = 1000
     private val permissions = arrayOf(
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.ACCESS_COARSE_LOCATION
     )
+    private var map: GoogleMap? = null
 
+    @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-
-//        Log.d("callback : ","centerList size : ${centerList.size}")
+        var latLng = LatLng(37.33,126.59)
+        map = googleMap
 
         if (!viewModel.centerList.value.isNullOrEmpty()) {
             for (center in viewModel.centerList.value!!) {
@@ -64,21 +69,24 @@ class MapsFragment : Fragment() {
             }
         }
 
-        googleMap.uiSettings.isZoomControlsEnabled=true
-        googleMap.uiSettings.isZoomGesturesEnabled=true
-        googleMap.setOnMarkerClickListener {
-            Toast.makeText(context,it.title,Toast.LENGTH_SHORT).show()
-            false
-        }
-        googleMap.moveCamera(
-            CameraUpdateFactory.newLatLngZoom(
-                LatLng(
-                    37.5,
-                    127.5
-                ),15F
+        googleMap.apply {
+            uiSettings.isZoomControlsEnabled=true
+            uiSettings.isZoomGesturesEnabled=true
+            if (ContextCompat.checkSelfPermission(
+                    ApplicationContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED) isMyLocationEnabled=true
+
+            setOnMarkerClickListener {
+                Toast.makeText(context,it.title,Toast.LENGTH_SHORT).show()
+                false
+            }
+            moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    currentLocation,10F
+                )
             )
-        )
-        //여기에 room에서 불러온 리스트들 마커찍어서 보야줘야함.
+        }
     }
 
     override fun onCreateView(
@@ -92,68 +100,78 @@ class MapsFragment : Fragment() {
 //        return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        checkPermission()
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
 
         viewModel.centerList.observe(viewLifecycleOwner, {
-
             Log.d("callback in : ","centerList size : ${it.size}")
-            if (!it.isNullOrEmpty()) {
 
+            if (!it.isNullOrEmpty()) {
                 Log.d("callback inn : ","centerList size : ${it.size}")
                 Log.d("centerList : ","${it[0].centerName}")
-//                centerList = it
+
                 mapFragment?.getMapAsync(callback)
-
             }
-
-
-
         })
+
         CoroutineScope(Dispatchers.IO).launch {
             viewModel.loadSavedCenterList()
         }
 
         mapFragment?.getMapAsync(callback)
-
     }
 
 
 
-//    fun checkPermission(){
-//        when {
-//            ContextCompat.checkSelfPermission(
-//                ApplicationContext(),
-//                Manifest.permission.ACCESS_FINE_LOCATION
-//            ) == PackageManager.PERMISSION_GRANTED -> {
-//                // You can use the API that requires the permission.
-//                performAction(...)
-//            }
-//            shouldShowRequestPermissionRationale(...) -> {
-//            // In an educational UI, explain to the user why your app requires this
-//            // permission for a specific feature to behave as expected. In this UI,
-//            // include a "cancel" or "no thanks" button that allows the user to
-//            // continue using your app without granting the permission.
-////            showInContextUI(...)
-//            Toast.makeText(activity,"권한요청을 승인해야 사용 가능합니다",Toast.LENGTH_SHORT).show()
-//        }
-//            else -> {
-//                // You can directly ask for the permission.
-//                ActivityCompat.requestPermissions(ApplicationContext(),
-//                    permissions,
-//                    REQUEST_CODE)
-//            }
-//        }
-//    }
+    private fun checkPermission(){
+        when {
+            ContextCompat.checkSelfPermission(
+                ApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+                fusedLocationProviderClient =  LocationServices.getFusedLocationProviderClient(requireActivity())
+                val task = fusedLocationProviderClient.lastLocation
+                task.addOnSuccessListener { location ->
+                    location?.let {
+                        currentLocation = LatLng(location.latitude,location.longitude)
+                        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,10F))
+                        map?.isMyLocationEnabled=true
+                    }
+                }
+                Toast.makeText(context, "Permissions Granted",Toast.LENGTH_SHORT).show()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected. In this UI,
+            // include a "cancel" or "no thanks" button that allows the user to
+            // continue using your app without granting the permission.
+//            showInContextUI(...)
+            Toast.makeText(activity,"권한요청을 승인해야 사용 가능합니다",Toast.LENGTH_SHORT).show()
+        }
+            else -> {
+                // You can directly ask for the permission.
+                ActivityCompat.requestPermissions(this.requireActivity(),
+                    permissions,
+                    REQUEST_CODE)
+            }
+        }
+    }
 
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        Toast.makeText(activity,"권한요청 성공",Toast.LENGTH_SHORT).show()
-//    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Toast.makeText(activity,"권한요청 성공",Toast.LENGTH_SHORT).show()
+    }
 }
--- app/src/main/java/com/example/covidmap/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/covidmap/ui/splash/SplashFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.NavHostFragment
@@ -17,40 +18,26 @@ import kotlinx.coroutines.*
 class SplashFragment : Fragment() {
 
     private val viewModel: SplashViewModel by viewModels()
-    private var currentPage = 1
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val binding = FragmentSplashBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        println(">>>>>>>>>>>>> splash onViewCreated >>>>>>>>>>")
 
-
-//            val response = viewModel.loadCenterList(currentPage)
-//            println(response.message().toString())
-//            if (response.isSuccessful){
-//                response.body()?.data?.let { viewModel.saveCenterList(it)
-//                currentPage++}
-//            }
         viewModel.centerList.observe(viewLifecycleOwner, {
-            //리스트에게 알려준당
             viewModel.saveCenterList(it)
         })
 
-//        CoroutineScope(Dispatchers.IO).launch {
-//            viewModel.currentPage.value?.let { viewModel.loadCenterList(it) }
-//        }
         viewModel.currentPage.observe(viewLifecycleOwner,{
             if (it>=10){
-                println("@@@@@@@@@@@@@@살려줘,,,@@@@@@@@@@@@@@2")
                 findNavController().navigate(R.id.action_splashFragment_to_mapsFragment)
             }
             CoroutineScope(Dispatchers.IO).launch {
@@ -58,12 +45,6 @@ class SplashFragment : Fragment() {
                 delay(50)
             }
         })
-
-
-
-
-
-
 
     }
 }
